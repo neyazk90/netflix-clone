7b2aa23b3c512614b35619d020504fe26b6a4d7a
Neyaz <neyaz.khan@shiprocket.com>
2024-09-08 12:50:18 +0530

    Browser page and Header added

--- src/components/UI/Layout.tsx/Header.tsx
+++ src/components/UI/Layout.tsx/Header.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import { Link } from "react-router-dom"
+import Language from "../../shared/Language"
+import Button from "../Button"
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+      <header className="bg-gradient-to-b from-black via-transparent to-transparent flex justify-between items-center px-6 py-2 text-white">
+      <div className="w-1/12">
+        <Link to="/">
+            <img className="w-full" src="/logo_icon.svg" alt="" />
+        </Link>
+          </div>
+          <div>
+              <Language/>
+              <Button btnText="Login" />
+          </div>
+      </header>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import { APP_ROUTES } from './Routes.tsx'
+
+
+
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={APP_ROUTES} />
+  </React.StrictMode>
+);
